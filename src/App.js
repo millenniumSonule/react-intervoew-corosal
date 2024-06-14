d3fe6a23854a6cbf7be9424c5d96b959e0dfab9c
millenniumSonule <anshumansonuley@gmail.com>
2024-06-14 20:15:22 +0530

    .

--- src/App.js
+++ src/App.js
@@ -6,7 +6,11 @@ const App = () => {
   
   return (
     <div>
-      {JSON.stringify(data)}
+        {data.map((item,index) => (
+          <div key={index}>
+            {item.email}
+          </div>
+        ))}
     </div>
   )
 }
