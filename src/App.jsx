46feb823c83d2ba87231d7270dda55a4f49a7c93
millenniumSonule <anshumansonuley@gmail.com>
2024-06-20 23:47:22 +0530

    .

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+import FaqComponent from './FaqComponent'
+
+const App = () => {
+  
+  return (
+    <div className='container'>
+         <div className='faq-container'>
+            <FaqComponent/>
+         </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/FaqComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import data from './data.json'
+
+const FaqComponent = () => {
+  return (
+    <div>
+        {/* {JSON.stringify(data[0].answer)} */}
+        
+    </div>
+  )
+}
+
+export default FaqComponent
