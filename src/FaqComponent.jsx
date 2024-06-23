f59cfcf4f262bc849af64c84c6ca41ad75aff791
millenniumSonule <anshumansonuley@gmail.com>
2024-06-23 11:50:07 +0530

    .

--- src/FaqComponent.jsx
+++ src/FaqComponent.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import data from './data.json'
-
+import './App.css'
 const FaqComponent = () => {
   return (
     <div>
-        {/* {JSON.stringify(data[0].answer)} */}
-        
+     
+      <div style={{display:'flex', justifyContent:'center',alignItems:'center',flexDirection:'column'}}>
+        {data.map((item, index) => (
+          <div key={index}>
+              {/* <p>{JSON.stringify(data[index].answer)}</p> */}
+              {/* <p>{JSON.stringify(data[index].question)}</p> */}
+              <div className='que-box'>
+                  {JSON.stringify(data[index].question)}
+              </div>
+            </div>
+          ))}
+        </div>
     </div>
   )
 }
