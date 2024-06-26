9ca2624dccb1990c86106278b057737e85e747d1
millenniumSonule <anshumansonuley@gmail.com>
2024-06-26 20:57:03 +0530

    .

--- src/FaqComponent.jsx
+++ src/FaqComponent.jsx
@@ -1,19 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import data from './data.json'
 import './App.css'
 const FaqComponent = () => {
+
+  const[isOpen,setOpen] = useState(0);
+
   return (
     <div>
      
       <div style={{display:'flex', justifyContent:'center',alignItems:'center',flexDirection:'column'}}>
         {data.map((item, index) => (
           <div key={index}>
-              {/* <p>{JSON.stringify(data[index].answer)}</p> */}
-              {/* <p>{JSON.stringify(data[index].question)}</p> */}
+
               <div className='que-box'>
-                  {JSON.stringify(data[index].answer)}
+                  {JSON.stringify(data[index].question)}
                   <button>{'> '}</button>
               </div>
+                <div className='answer-box'>
+                {JSON.stringify(data[index].answer)}
+                </div>
             </div>
           ))}
         </div>
